d2e1196e7820e2b34be8f4252b183df56eda2ce4
Achmad Mahardi <achmad@mahardi.me>
2013-11-14 09:53:28 +0700

    add support for pre-populated fields, thanks to @BenBish

--- jvfloat.js
+++ jvfloat.js
@@ -1,6 +1,6 @@
 /*
  * JVFloat.js
- * modified on: 11/12/2013
+ * modified on: 11/14/2013
  */
 
 (function($) {
@@ -20,7 +20,7 @@
 				$(this).before('<span class=placeHolder>' + placeholderText);
 			}
 			// checks to see if inputs are pre-populated and adds active to span.placeholder
-			if ($(this).val() != '') {
+			if ($(this).val() !== '') {
 				$(this).siblings('.placeHolder').addClass('active');
 				$(this).parent().addClass('active');
 			}
