91909a33e57b483fd367b85b78183f09fa9aaf0c
maarten.krijn <mrsnowflake@gmail.com>
2014-01-29 21:32:47 +0100

    Converted spans to actual labels.
    Instead of the spans being used as improvised labels, actual labels are
    now created. Input elements' id's are used as for attribute value when
    present, otherwise a 4 character id is used. A check is implemented, to
    make sure the 4 character generated id is unique within the dom.

--- jvfloat.js
+++ jvfloat.js
@@ -4,6 +4,23 @@
  */
 
 (function($) {
+  "use strict";
+  
+  var generateUIDNotMoreThan1million = function() {
+    do {
+      var id = ("0000" + (Math.random()*Math.pow(36,4) << 0).toString(36)).substr(-4);
+    } while (!!$('#'+id).length);
+    
+    return id;
+  };
+  var createIdOnElement = function($el) {
+    var id = generateUIDNotMoreThan1million();
+    
+    $el.prop('id', id);
+    
+    return id;
+  };
+  
   // Init Plugin Functions
   $.fn.jvFloat = function() {
     // Check input type - filter submit buttons.
@@ -11,10 +28,15 @@
       // Wrap the input in div.jvFloat
       var $el = $(this)
         .wrap('<div class=jvFloat>');
+      
+      var forId = $el.attr('id');
+      if (!forId)
+        forId = createIdOnElement($el);
+      
       // Store the placeholder text in span.placeHolder
       // added `required` input detection and state
       var required = $el.attr('required') || '';
-      var placeholder = $('<span class="placeHolder ' + required + '">' + $el.attr('placeholder') + '</span>')
+      var placeholder = $('<label class="placeHolder ' + required + '" for="'+forId+'">' + $el.attr('placeholder') + '</label>')
         .insertBefore($el);
       // checks to see if inputs are pre-populated and adds active to span.placeholder
       setState();
